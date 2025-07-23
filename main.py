937d63af326b45eeec7e58d6c2b929633db19ce1
BenAsama <asamaben92@gmail.com>
2025-07-23 19:51:53 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app=FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-# this is a comment line but i cant't
-boy = app()
+def root():
+    return {"message": "my api link !!!!!!!!!!!"}
+
+@app.get("/post")
+def get_posts():
+    return {"data" : "this is ur post "}
+
+@app.post("/create_post")
+def create_post(payload: dict = Body(...)):
+    return{"message": "creating post request"}
